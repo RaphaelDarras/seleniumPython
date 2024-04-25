8336678dea3a11ad432bbb4efdacaeccf10454cc
Raphael <raphael.darras.pro@gmail.com>
2024-04-25 21:10:15 +0200

    Adding comments

--- tests/test_tp.py
+++ tests/test_tp.py
@@ -20,9 +20,10 @@ itemsAmountCart = ''
 itemsPricesCart = []
 
 @scenario('./features/tp.feature','Exercise 1')
-# def test_login_logout(browser):
-#     pass
+def test_login_logout(browser):
+    pass
 
+#Make this step reusable easily for other users with a variable
 @given(parsers.parse('I am logged as "{user}"'))
 def standard_user_login(browser,user):
     browser.get('https://www.saucedemo.com/')
@@ -35,6 +36,7 @@ def standard_user_login(browser,user):
 @when('I logout')
 def logout(browser):
     browser.find_element(By.ID, 'react-burger-menu-btn').click()
+    #Problem specific to Chrome: if we don't wait a little the button is not clickable
     logout_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'logout_sidebar_link')))
     logout_button.click()
 
@@ -45,14 +47,15 @@ def check_login_page_displayed(browser):
     assert browser.find_element(By.ID, 'login-button').is_displayed()
 
 @scenario('./features/tp.feature','Exercise 2')
-# def test_bad_credentials_error(browser):
-#     pass
+def test_bad_credentials_error(browser):
+    pass
 
 
 @given('I am on the login page')
 def access_login_page(browser):
     browser.get('https://www.saucedemo.com/')
 
+#Make this step reusable easily for other users with a variable
 @when(parsers.parse('I try to log with the "{user}" credentials'))
 def locked_out_user_log(browser,user):
     browser.find_element(By.ID, 'user-name').send_keys(user)
@@ -68,34 +71,45 @@ def log_error_message(browser):
 def test_command(browser):
     pass
 
+#Make this step reusable easily for other sorting orders with a variable
 @when(parsers.parse('I sort the list in "{sort_order}" order'))
 def sort_items(browser,sort_order):
+    #Step 2: sorting the list
     sort_orders = Select(browser.find_element(By.CSS_SELECTOR, 'select[data-test=product-sort-container]'))
     sort_orders.select_by_value(sort_order)
 
+#Make this step reusable easily for other items amount with a variable
 @when(parsers.parse('I add the first "{amount}" items in the cart'))
 def add_two_items(browser, amount):
-    global itemsAmountCart, itemsNamesCart, itemsPricesCart
+    global itemsAmountCart, itemsPricesCart
+    #Saving the amount coming from the step definition in a global variable to easily reuse it later
+    #And keep the reusability of the whole step
     itemsAmountCart = amount
     items=browser.find_elements(By.CLASS_NAME, "btn_inventory")
-    itemsNames=browser.find_elements(By.CSS_SELECTOR, "div[data-test=inventory-item-name]")
     itemsPrices=browser.find_elements(By.CSS_SELECTOR, "div[data-test=inventory-item-price]")
+    #Step 3: adding the first 2 items to the cart
+    #And saving the prices in a global variable to use them in the bonus step
+    #This is pure Python, not Selenium. Go ask Google about range an for loops in Python :)
     for x in range(int(amount)):
         items[x].click()
         itemsPricesCart.append(itemsPrices[x].text)
+    #Little bonus, making sure the little red bubble on the cart shows the correct number
     assert browser.find_element(By.CSS_SELECTOR, "span[data-test=shopping-cart-badge]").text == amount
 
 
 @when('I go to the cart')
 def go_cart(browser):
+    #Step 4: Going to the cart
     browser.find_element(By.CSS_SELECTOR, 'a[data-test=shopping-cart-link]').click()
     assert browser.current_url == 'https://www.saucedemo.com/cart.html'
     assert browser.find_element(By.CSS_SELECTOR, 'div[data-test=cart-list]').is_displayed()
     cartItems = browser.find_elements(By.CLASS_NAME, 'cart_item')
+    #Step 5: making sure we have 2 products in the cart
     assert len(cartItems) == int(itemsAmountCart)
 
 @when(parsers.parse('I order as "{firstname}" "{lastname}" habitant au "{zipcode}"'))
 def order(browser, firstname, lastname, zipcode):
+    #Step 6: Add client informations
     browser.find_element(By.ID, 'checkout').click()
     assert browser.current_url == 'https://www.saucedemo.com/checkout-step-one.html'
     browser.find_element(By.ID, 'first-name').send_keys(firstname)
@@ -107,12 +121,19 @@ def order(browser, firstname, lastname, zipcode):
 
 @when('order informations are correct')
 def check_order_infos(browser):
+    #Step 7: Bonus !
+    #Lot of things to ask google here
+    #string[1:]
+    #float()
+    #string.replace('','')
+    #round()
     firstItemPrice = itemsPricesCart[0][1:]
     secondItemPrice = itemsPricesCart[1][1:]
     itemsTotal = float(firstItemPrice)+float(secondItemPrice)
     assert itemsTotal == float(browser.find_element(By.CSS_SELECTOR, "div[data-test=subtotal-label]").text.replace('Item total: $', ''))
 
     roundedTotal = round(itemsTotal)
+    #The tax rate was not explained anywhere, I had to discover it myself: it's 8% of the subtotal
     tax = roundedTotal*0.08
     assert tax == float(browser.find_element(By.CSS_SELECTOR, "div[data-test=tax-label]").text.replace('Tax: $', ''))
 
@@ -121,11 +142,13 @@ def check_order_infos(browser):
 
 @when('I finalize the order')
 def finalize_order(browser):
+    #Step 8
     browser.find_element(By.ID, 'finish').click()
     assert browser.current_url == 'https://www.saucedemo.com/checkout-complete.html'
 
 @then('the order is confirmed')
 def confirmed_order(browser):
+    #Step 9
     assert browser.find_element(By.CSS_SELECTOR, 'img[data-test=pony-express]').is_displayed()
     assert browser.find_element(By.CSS_SELECTOR, 'h2[data-test=complete-header]').text == 'Thank you for your order!'
     assert browser.find_element(By.CSS_SELECTOR, 'div[data-test=complete-text]').text == 'Your order has been dispatched, and will arrive just as fast as the pony can get there!'
